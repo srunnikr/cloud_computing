ddfd4da9cb78f3355f7ac290aaa4cb378dde80ce
Dhruv Sharma <dhsharma@cs.ucsd.edu>
2016-10-10 17:55:52 -0700

    Added portland module under src directory for all Portland-related implementations.

---
+++ ntu-dsi-dcn/src/portland/helper/portland-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __PORTLAND_HELPER_H__
+#define __PORTLAND_HELPER_H__
+
+#include "ns3/portland.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __PORTLAND_HELPER_H__ */
+
---
+++ ntu-dsi-dcn/src/portland/model/portland.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __PORTLAND_H__
+#define __PORTLAND_H__
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __PORTLAND_H__ */
+
