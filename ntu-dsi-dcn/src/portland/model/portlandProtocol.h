451ba6e8f766c930ac1f6778411e8577360d2fc9
Sreejith Unnikrishnan <sreejithu4u@gmail.com>
2016-10-18 13:16:52 -0700

    A few changes for portland FM

---
+++ ntu-dsi-dcn/src/portland/model/portlandProtocol.h
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+
+// Packet types
+enum PACKET_TYPE {
+	PKT_MAC_REGISTER = 1,
+	PKT_ARP_REQUEST,
+	PKT_ARP_RESPONSE,
+	PKT_ARP_FLOOD
+};
