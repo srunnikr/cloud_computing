81ecda493116d42928cc95817e0f815bbcc0fd3c
bdaemon <amit.borase@yahoo.in>
2016-11-05 15:29:55 -0700

    first-cut docker, need to automate few more things

---
+++ facebook_haystack/setup/haystack_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# clean existing images and containers
+echo "****************************"
+echo "*  CLEANUP EXISTING STUFF  *"
+echo "****************************"
+docker rm -f store
+docker rm -f cache
+docker rm -f dir
+docker rm -f balancer
+docker rm -f server1
+docker rm -f server2
+
+docker rmi -f haystack_store
+docker rmi -f haystack_cache
+docker rmi -f haystack_dir
+docker rmi -f load_balancer
+docker rmi -f web_server
+
+docker network rm haynet
+
+# create custom network off 'bridge'
+echo "********************************"
+echo "*   CREATE A CUSTOM NETWORK    *"
+echo "********************************"
+
+docker network create -d bridge haynet
+
+# build all the images
+echo "*******************************"
+echo "*    BUILD ALL THE IMAGES     *"
+echo "*******************************"
+docker build -t haystack_store ./haystack_store/
+docker build -t haystack_cache ./haystack_cache/
+docker build -t haystack_dir ./haystack_dir/
+docker build -t load_balancer ./load_balancer/
+docker build -t web_server ./web_server/
+
+# Start all the container instances
+echo "**************************************"
+echo "*    START ALL CONTAINER INSTANCES   *"
+echo "**************************************"
+docker run -itd --name store haystack_store
+docker run -itd --name cache haystack_cache
+docker run -itd --name dir haystack_dir
+docker run -itd --name server1 web_server
+docker run -itd --name server2 web_server
+docker run -itd --name balancer load_balancer
+
