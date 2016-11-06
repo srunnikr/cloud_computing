a49740c82857c2d7958b5d6e7d2d76f5d7a96a72
Khalid <khalid10@users.noreply.github.com>
2016-11-05 20:08:41 -0700

    Add web server API endpoints

--- facebook_haystack/setup/web_server/server.js
+++ facebook_haystack/setup/web_server/server.js
@@ -1,15 +1,42 @@
-
 'use strict';
 
 const express = require('express');
+const request = require('request');
 
-// Constants
+const directory = 'http://localhost:8080'; // PLACEHOLDER
+const dataStore = 'http://localhost:8080'; // PLACEHOLDER
 const PORT = 8080;
-
-// App
 const app = express();
+
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('Haystack photos\n');
+});
+
+app.get('/photos/:photo_id', function(req, res) {
+  // get the URL from the directory
+  request(dataStore + '/placeholder', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var url = body;
+
+	// return the response to the browser
+	res.send(url);
+    }
+  });
+});
+
+app.delete('/photos/:photo_id/delete', function(req, res) {
+  // TODO
+    res.send("Photo id (DELETE): " + req.params.photo_id);
+ });
+
+app.post('/photos', function(req, res) {
+  // TODO: post to dataStore
+  res.send("Photo upload (POST)");
+});
+
+app.get('/placeholder', function(req, res) {
+  // place holder for the directory URL
+  res.send('http://cache/machine_id/logical_id/photo');
 });
 
 app.listen(PORT);
