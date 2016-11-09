7b4c6961d3879fc5b1c52f3a478392cbcb007453
Khalid <khalid10@users.noreply.github.com>
2016-11-08 22:07:34 -0800

    Add web server to store connection

--- facebook_haystack/setup/web_server/server.js
+++ facebook_haystack/setup/web_server/server.js
@@ -2,11 +2,34 @@
 
 const express = require('express');
 const request = require('request');
-
 const directory = 'http://localhost:8080'; // PLACEHOLDER
-const dataStore = 'http://localhost:8080'; // PLACEHOLDER
-const PORT = 8080;
 const app = express();
+const PORT = 8080;
+
+/* ######## store setup   ######## */
+const dataStore = '192.168.1.1';
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: [dataStore] });
+
+// connect to the database
+client.connect(function (err) {
+  if (err) return console.error(err);
+  console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+});
+
+// create the keyspace
+const q = "CREATE KEYSPACE IF NOT EXISTS cse291 WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\' : 1}"
+client.execute(q, function (err, result) {
+    if (err) return console.error(err);
+    console.log("Created the keyspace")
+});
+
+// create the table (temporary schema)
+client.execute('CREATE TABLE IF NOT EXISTS cse291.photos(id int PRIMARY KEY, data blob)', function (err, result) {
+    if (err) return console.error(err);
+    console.log("Created the table");
+});
+/* ######## end of store setup   ######## */
 
 app.get('/', function (req, res) {
   res.send('Haystack photos\n');
@@ -40,4 +63,4 @@ app.get('/placeholder', function(req, res) {
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Web server is running on port: ' + PORT);
